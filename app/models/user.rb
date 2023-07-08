5d95ba90785f53ff6f2c972c673953575831787e
Logan Cole <emtlogancole@gmail.com>
2023-07-08 16:47:59 -0500

    fix: user validates member boolean correctly
    Co-authored-by: Reid Miller <49500224+reidsmiller@users.noreply.github.com>
    Co-authored-by: Alejandro Lopez <121198380+AlejandroLopez1992@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :email
-  validates :member, inclusion: [true, false]
+  validates :member, exclusion: { :in => [nil], :message => "can't be blank" }
   validates_uniqueness_of :email
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,6 +6,6 @@ RSpec.describe User, type: :model do
     it { should validate_presence_of(:last_name) }
     it { should validate_presence_of(:email) }
     it { should validate_uniqueness_of(:email)}
-    it { should validate_inclusion_of(:member) }
+    it { is_expected.to validate_presence_of(:member) }
   end
 end
