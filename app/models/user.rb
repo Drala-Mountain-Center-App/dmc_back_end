9dba35f50b4afe5ec3b59406214dc71e6fec1a60
Logan Cole <emtlogancole@gmail.com>
2023-07-08 16:45:39 -0500

    fix: user validates uniqueness of email
    Co-authored-by: Reid Miller <49500224+reidsmiller@users.noreply.github.com>
    Co-authored-by: Alejandro Lopez <121198380+AlejandroLopez1992@users.noreply.github.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-  validates_presence_of :first_name, :last_name, :email, :member
+  validates_presence_of :first_name, :last_name, :email
+  validates :member, inclusion: [true, false]
+  validates_uniqueness_of :email
 end
