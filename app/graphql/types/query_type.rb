e870e4d9dc3dac1fb980347ad651038e5e5f70bd
Logan Cole <emtlogancole@gmail.com>
2023-07-08 17:19:27 -0500

    feat: add all_user fields to graphql; delete graphiql interface
    Co-authored-by: Reid Miller <49500224+reidsmiller@users.noreply.github.com>
    Co-authored-by: Alejandro Lopez <121198380+AlejandroLopez1992@users.noreply.github.com>

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,12 +2,10 @@ module Types
   class QueryType < Types::BaseObject
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    field :all_users, [UserType], null: false
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    def all_users
+      User.all
     end
   end
 end
