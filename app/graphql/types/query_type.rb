c6608b795edc8981abef75e4185000882d1fd4e4
Alejandro Lopez <alejandrolopez1592@gmail.com>
2023-07-08 17:49:12 -0600

    added graphQL methods to find user by ID and email

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,5 +7,21 @@ module Types
     def all_users
       User.all
     end
+
+    field :user_by_email, Types::UserType, null: false, description: "Gets one user by email" do
+      argument :email, String, required: true
+    end
+
+    def user_by_email(email)
+      User.find_by(email)
+    end
+
+    field :user, Types::UserType, null: false, description: "Gets one user by ID" do
+      argument :id, ID, required: true
+    end
+
+    def user(id:)
+      User.find(id)
+    end
   end
 end
