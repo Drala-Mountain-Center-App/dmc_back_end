c57eae7439d0f5f19debbab7af54e00bc34742b3
Reid Miller <reid.s.miller@gmail.com>
2023-07-08 20:55:56 -0400

    fix: Fixed accidental deletion of relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
   validates_presence_of :first_name, :last_name, :email
   validates :member, exclusion: { :in => [nil], :message => "can't be blank" }
   validates_uniqueness_of :email
+  has_many :meditations, dependent: :destroy
 end
