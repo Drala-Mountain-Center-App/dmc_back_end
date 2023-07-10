d8dfebd2c29a1be231f59f12920a0f2a380afbce
Reid Miller <reid.s.miller@gmail.com>
2023-07-10 15:22:14 -0400

    graphql: Add CreateMeditation mutation

---
+++ app/graphql/mutations/create_meditation.rb
@@ -0,0 +1,14 @@
+module Mutations
+  class CreateMeditation < BaseMutation
+    argument :total_sitting_time, Integer, required: true
+    
+    type Types::MeditationType
+
+    def resolve(description: nil, total_sitting_time: nil)
+      Meditation.create!(
+        total_sitting_time: total_sitting_time,
+        user: context[:current_user]
+      )
+    end
+  end
+end
